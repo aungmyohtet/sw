122422aff2dee59ee2b8c97fc703bd0e08b81ce7
hninyadanar <hnin.yadanar.win@frobom.com>
2017-07-07 13:57:52 +0630

    update alert_word_counts

--- src/main/java/com/frobom/sw/web/ResultController.java
+++ src/main/java/com/frobom/sw/web/ResultController.java
@@ -1,5 +1,8 @@
 package com.frobom.sw.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -7,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.frobom.sw.entity.Mail;
 import com.frobom.sw.service.AlertWordCountService;
 import com.frobom.sw.service.MailCountService;
 import com.frobom.sw.service.MailService;
@@ -52,8 +56,14 @@ public class ResultController {
 
     @RequestMapping(value = "/alert_word_counts", method = RequestMethod.GET)
     public String ShowAlertWordCount(Model model) {
-        model.addAttribute("mails", mailService.findAllByFetchingSubEntities());
-        model.addAttribute("alertWordCounts",alertWordCountService.findAll());
+        List<Mail> mails = mailService.findAllByFetchingSubEntities();
+        List<Mail> mailsWithAlertWordCount = new ArrayList<>();
+        for (Mail mail : mails) {
+            if (mail.getAlertWordCount() != null) {
+                mailsWithAlertWordCount.add(mail);
+            }
+        }
+        model.addAttribute("mails", mailsWithAlertWordCount);
         return "alert_word_counts";
     }
 }
